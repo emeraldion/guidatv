aefee136bfd871edb19e4a3816ec17ac58772642
Claudio Procida <claudiopro@users.noreply.github.com>
2007-06-13 18:39:52 +0000

    Updated Dashboard Widget in response to the recent changes in the source format HTML.

--- Dashboard Widget/GuidaTV.wdgt/Remote.js
+++ Dashboard Widget/GuidaTV.wdgt/Remote.js
@@ -1,19 +1,22 @@
 /**
  *	GuidaTV Widget
  *
- *	© Claudio Procida 2005
+ *	Copyright 2005-2007 Claudio Procida. All rights reserved.
+ *	http://www.emeraldion.it
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  Bug fixes, suggestions and comments should be sent to:
+ *  claudio@emeraldion.it
  *
- *	Disclaimer
- *
- *	The GuidaTV Widget software (from now, the "Software") and the accompanying materials
- *	are provided “AS IS” without warranty of any kind. IN NO EVENT SHALL THE AUTHOR(S) BE
- *	LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES,
- *	INCLUDING LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN
- *	IF THE AUTHOR(S) HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. The entire risk as to
- *	the results and performance of this software is assumed by you. If the software is
- *	defective, you, and not Claudio Procida, assume the entire cost of all necessary servicing,
- *	repairs and corrections. If you do not agree to these terms and conditions, you may not
- *	install or use this software.
  */
  
 var sources = {
